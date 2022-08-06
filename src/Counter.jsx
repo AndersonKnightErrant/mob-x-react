35beb213f2de6454812b01ad89272240864dc396
AndersonKnightErrant <andrew83anderson@gmail.com>
2022-08-06 17:55:46 +0400

    add store for save state mob-x

---
+++ src/Counter.jsx
@@ -0,0 +1,10 @@
+const Counter = () => {
+	return (
+		<div className="counter">
+			<button className="btn">+</button>
+			<button className="btn">-</button>
+		</div>
+	);
+}
+
+export default Counter;
